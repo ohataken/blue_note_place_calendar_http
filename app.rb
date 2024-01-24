fd147a82aa39db21dc832e92ee05e50047da6465
ohataken <11092425+ohataken@users.noreply.github.com>
2024-01-24 23:08:50 +0900

    update app.rb

--- app.rb
+++ app.rb
@@ -1,1 +1,13 @@
 require "sinatra"
+require "uri"
+require "open-uri"
+require "nokogiri"
+
+get "/live/:cal" do
+  origin_host = ENV["ORIGIN_HOST"]
+  cal = params["cal"]
+  uri = URI("#{origin_host}/live/?cal=#{cal}")
+  html_doc = Nokogiri::HTML(URI.open(uri))
+  cards = html_doc.css("div.calBoardCardsBody > div.c_calBoardCard")
+  cards.to_xml
+end
