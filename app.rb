3cc6d0c762b2fb509318ea6e6133afe98c398862
ohataken <11092425+ohataken@users.noreply.github.com>
2024-01-26 01:08:11 +0900

    update app.rb

--- app.rb
+++ app.rb
@@ -2,29 +2,14 @@ require "sinatra"
 require "uri"
 require "open-uri"
 require "nokogiri"
+require "./calendar_card"
 
 get "/live/:cal" do
   origin_host = ENV["ORIGIN_HOST"]
   cal = params["cal"]
   uri = URI("#{origin_host}/live/?cal=#{cal}")
   html_doc = Nokogiri::HTML(URI.open(uri))
-  cards = html_doc.css("div.calBoardCardsBody > div.c_calBoardCard")
-
-  days = cards.map do |card|
-    a = card.at_css("a")
-    day = card.at_css("a > div.m_calCardHead > div.m_calCardDay")
-    image = card.at_css("a > div.m_calCardHead > div.m_calCardImage > img")
-    body = card&.at_css("a > div.m_calCardBody > p.m_calCardText")
-    cat = card.at_css("a > div.m_calCardBody > span.m_calCardCat")
-
-    {
-      a_href: a && a["href"],
-      day_content: day&.content,
-      image_src: image && image["src"],
-      body_content: body&.content,
-      cat_content: cat&.content,
-    }
-  end
-
-  days.to_json
+  nodeset = html_doc.css("div.calBoardCardsBody > div.c_calBoardCard")
+  cards = CalendarCard.from_nodeset nodeset
+  cards.to_json
 end
