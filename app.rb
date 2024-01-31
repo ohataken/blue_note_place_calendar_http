4788b02e05903bf1e656a0563338758c7c043c45
ohataken <11092425+ohataken@users.noreply.github.com>
2024-01-31 12:36:48 +0900

    update app.rb

--- app.rb
+++ app.rb
@@ -2,14 +2,17 @@ require "sinatra"
 require "uri"
 require "open-uri"
 require "nokogiri"
+require "./calendar_cache"
 require "./calendar_card"
 
 get "/live/:cal" do
   origin_host = ENV["ORIGIN_HOST"]
   cal = params["cal"]
   uri = URI("#{origin_host}/live/?cal=#{cal}")
-  html_doc = Nokogiri::HTML(URI.open(uri))
-  nodeset = html_doc.css("div.calBoardCardsBody > div.c_calBoardCard")
+  nodeset = CalendarCache.cache cal.to_s do
+    html_doc = Nokogiri::HTML(URI.open(uri))
+    html_doc.css("div.calBoardCardsBody > div.c_calBoardCard")
+  end
   cards = CalendarCard.from_nodeset nodeset
   cards.to_json
 end
